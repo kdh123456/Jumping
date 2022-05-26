d1fca97f33109d911a9f8eeb73cfe0871087a04b
kjp2981 <81199996+kjp2981@users.noreply.github.com>
2022-05-26 09:06:23 +0900

    약간 수정

--- Assets/02. Script/3.Effect/MapleBlock.cs
+++ Assets/02. Script/3.Effect/MapleBlock.cs
@@ -18,13 +18,14 @@ public class MapleBlock : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Sequence seq = DOTween.Sequence();
-        seq.AppendCallback(() => StartCoroutine(ShakeCoroutine()));
-        seq.SetDelay(.1f);
-        seq.Append(spriteRenderer.DOFade(0, .2f));
+        
 
         if (collision.gameObject.CompareTag("Player"))
         {
+            Sequence seq = DOTween.Sequence();
+            seq.AppendCallback(() => StartCoroutine(ShakeCoroutine()));
+            seq.SetDelay(.1f);
+            seq.Append(spriteRenderer.DOFade(0, .2f));
             //this.GetComponent<SpriteRenderer>().DOFade(0, .3f);
             //StartCoroutine(ShakeCoroutine());
             seq.Play().SetAutoKill();
@@ -33,13 +34,14 @@ public class MapleBlock : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Sequence seq = DOTween.Sequence();
-        seq.Append(spriteRenderer.DOFade(1, .2f));
-        seq.SetDelay(.2f);
-        seq.AppendCallback(() => StartCoroutine(ShakeCoroutine()));
+        
 
         if (collision.gameObject.CompareTag("Player"))
         {
+            Sequence seq = DOTween.Sequence();
+            seq.Append(spriteRenderer.DOFade(1, .2f));
+            seq.SetDelay(.2f);
+            seq.AppendCallback(() => StartCoroutine(ShakeCoroutine()));
             //StartCoroutine(ShakeCoroutine());
             //this.GetComponent<SpriteRenderer>().DOFade(1, .3f);
             seq.Play().SetAutoKill();
@@ -49,9 +51,9 @@ public class MapleBlock : MonoBehaviour
     private IEnumerator ShakeCoroutine()
     {
         material.SetFloat("_WindSpeed", 2f);
-        material.SetFloat("_WindIntensity", 1f);
+        material.SetFloat("_WindIntensity", .5f);
         yield return waitForSeconds01;
         material.SetFloat("_WindSpeed", 1f);
-        material.SetFloat("_WindIntensity", 0.5f);
+        material.SetFloat("_WindIntensity", 0.1f);
     }
 }
