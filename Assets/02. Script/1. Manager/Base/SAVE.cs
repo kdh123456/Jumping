a2d952ec7d9b102d32dfe0bce3194f205911c2e0
kjp2981 <81199996+kjp2981@users.noreply.github.com>
2022-06-16 15:53:04 +0900

    리더보드 수정 및 버그 수정

--- Assets/02. Script/1. Manager/Base/SAVE.cs
+++ Assets/02. Script/1. Manager/Base/SAVE.cs
@@ -7,6 +7,4 @@ public class SAVE
     public Vector2 position = new Vector2(-9f, 4f);
     public float timer = 0f;
     public bool isFirst = true;
-    //public List<float> timeList = null;
-    public float[] timeList = new float[10] { 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f };
 }
---
+++ Assets/02. Script/1. Manager/Base/TIMELIST.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TIMELIST
+{
+    public float[] timeList = new float[10] { 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f };
+}
