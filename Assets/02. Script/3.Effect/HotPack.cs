cef9a5d0801525edf9d9a848e217194e047b431b
kjp2981 <81199996+kjp2981@users.noreply.github.com>
2022-07-01 09:04:30 +0900

    핫팩 완성

--- Assets/02. Script/3.Effect/HotPack.cs
+++ Assets/02. Script/3.Effect/HotPack.cs
@@ -1,26 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class HotPack : MonoBehaviour
 {
     public float time = 9f;
     private float selectCountdown = 0f;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            spriteRenderer.DOColor(Color.red, time);
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (Mathf.Floor(selectCountdown) >= time)
+        if (collision.CompareTag("Player"))
         {
-            EventManager.TriggerEvent("Hotpack");
-            selectCountdown = 0f;
-        }
-        else
-        {
-            selectCountdown += Time.deltaTime;
+            if (Mathf.Floor(selectCountdown) >= time)
+            {
+                EventManager.TriggerEvent("Hotpack");
+                spriteRenderer.DOColor(Color.white, .2F);
+                selectCountdown = 0f;
+            }
+            else
+            {
+                selectCountdown += Time.deltaTime;
+            }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        selectCountdown = 0f;
+        if (collision.CompareTag("Player"))
+        {
+            selectCountdown = 0f;
+        }
     }
 }
